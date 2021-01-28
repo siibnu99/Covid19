e3d65ccb383ed1751321a1b5b7b7e603033c17fe
siibnu99 <55118281+siibnu99@users.noreply.github.com>
2021-01-28 19:49:26 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,5 +6,8 @@ const enforce = require('express-sslify');
 const app = express();
 
 app.use(enforce.HTTPS({trustProtoHeader:true}));
+app.use(serveStatic(__dirname + '/dist'));
+app.use(history());
+
 app.listen(process.env.PORT || 5000);
 
