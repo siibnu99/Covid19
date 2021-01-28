7c05c7a71ae169f1f2ff1bd326c38e4373df7888
siibnu99 <55118281+siibnu99@users.noreply.github.com>
2021-01-28 20:05:42 +0700

    Enable Cors

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
 const enforce = require('express-sslify');
+const cors = require('cors')
 
 const app = express();
 
 app.use(enforce.HTTPS({trustProtoHeader:true}));
 app.use(serveStatic(__dirname + '/dist'));
 app.use(history());
-
+app.use(cors());
 app.listen(process.env.PORT || 5000);
 
